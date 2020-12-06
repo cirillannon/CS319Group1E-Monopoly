86ca69b268ac60036b7ee6ae16247281773a0f4d
arnisafazla <arnisa.fazla@ug.bilkent.edu.tr>
2020-12-06 23:41:50 +0300

    added GameBoard, GameManager and GamesStateManager classes

--- sample/subsystems/Controller/GameManager.java
+++ sample/subsystems/Controller/GameManager.java
@@ -1,4 +1,57 @@
 package sample.subsystems.Controller;
 
+import sample.subsystems.GameLogic.GameBoard;
+// communicates with Server ?
 public class GameManager {
+    // properties
+
+    int playerStartMoney;
+    boolean gameStarted;
+    boolean gameOver;
+
+    // constructor
+    public Controller() {
+
+    }
+
+    // methods
+    public boolean initGame(int startMoney) {
+        setPlayersStartMoney(startMoney);
+        initBank();
+        setPawnStartPosition();
+        startPawnSelection();
+        // static or instance???
+        // initBoard();
+    }
+
+    // give players money - through gameBoard?
+    public void setPlayersStartMoney(int startMoney) {
+        this.playerStartMoney = startMoney;
+    }
+
+    // how to reach pawns?
+    public void setPawnStartPosition() {
+
+    }
+
+    //
+    public void startPawnSelection() {
+
+    }
+
+    public boolean initBank() {
+
+    }
+
+    public boolean startGame() {
+
+    }
+
+    public boolean endGame() {
+
+    }
+
+    public boolean isGameOver() {
+
+    }
 }
---
+++ sample/subsystems/GameLogic/GameBoard.java
@@ -0,0 +1,12 @@
+package sample.subsystems.GameLogic;
+
+public class GameBoard {
+    public void update() {
+
+    }
+
+    // static or should GameManager have an instance???
+    public static void initBoard() {
+
+    }
+}
