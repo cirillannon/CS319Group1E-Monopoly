f898998328ca037190bd8c3eff9a5b70b10132ea
Ege Türker <ege.turker@ug.bilkent.edu.tr>
2020-12-07 09:15:23 +0300

    Implemented Dice.java and Tile.java classes

--- sample/subsystems/GameLogic/Dice.java
+++ sample/subsystems/GameLogic/Dice.java
@@ -1,4 +1,28 @@
 package sample.subsystems.GameLogic;
 
 public class Dice {
+
+    private int diceTotal;
+    private boolean isDoubles;
+
+    public Dice(){
+        diceTotal = 0;
+        isDoubles = false;
+    }
+
+    public int getDiceTotal() {
+        return diceTotal;
+    }
+
+    public boolean getDoubles(){
+        return isDoubles;
+    }
+
+    public void rollDice(){
+        int d1 = (int)(Math.random()*6 + 1);
+        int d2 = (int)(Math.random()*6 + 1);
+
+        diceTotal = d1 + d2;
+        isDoubles = (d1 == d2);
+    }
 }
--- sample/subsystems/GameLogic/Tile.java
+++ sample/subsystems/GameLogic/Tile.java
@@ -1,4 +1,20 @@
 package sample.subsystems.GameLogic;
 
-public class Tile {
+public abstract class Tile {
+
+    private String tileName;
+    private int tileLocation;
+
+    public Tile ( String tName, int tLocation){
+        tileName = tName;
+        tileLocation = tLocation;
+    }
+
+    public String getTileName(){
+        return tileName;
+    }
+
+    public int getTileLocation() {
+        return tileLocation;
+    }
 }
