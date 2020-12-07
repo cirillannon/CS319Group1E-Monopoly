1b223634ffec71d1ba04d2f5731db264ee4ca1d8
furkanaygar <71967163+furkanaygar@users.noreply.github.com>
2020-12-07 17:53:59 +0300

    Update Bank.java

--- sample/subsystems/GameLogic/Bank.java
+++ sample/subsystems/GameLogic/Bank.java
@@ -1,31 +1,29 @@
 package sample.subsystems.GameLogic;
-public class Bank {
+
+public class Bank 
+{
     private int numberOfHouses;
     private int numberOfHotels;
     public Bank(){
         numberOfHouses= 32;
-        numberOfHotels=12;
+        numberOfHotels= 12;
     }
     // Add price variable for property.
     public boolean buyProperty(Property prp ,Tile t , Player p  ) {
-        if (!prp.isOwned() && p.location == t.tileLocation && p.getBalance() >= prp.price)  
+        if (!prp.isOwned() && p.location == t.tileLocation && p.getBalance() >= prp.price)
         {
             prp.setOwner(p);
-            return true; 
+            return true;
         }
         else
             return false;
-
     }
-    // Add player and playerOfferAmount parameters. 
+    // Add player and playerOfferAmount parameters.
     public void auctionProperty(Property prp ,Player p , int playerOfferAmount)  {
-        if (p.getBalance >= playerOfferAmount){
+        if (p.getBalance () >= playerOfferAmount){
             p.balance = p.balance- playerOfferAmount;
             prp.setOwner(p);
-            
         }
-        
-
     }
     // Add tradeAmount parameter and removeOwner() for property
     public boolean tradeProperty(Property prp, Player owner, Player target , int tradeAmount) {
@@ -36,11 +34,9 @@ public class Bank {
          target.balance = target.balance - tradeAmount;
          owner.balance = owner.balance + tradeAmount;
          return true;
-
         }
         else
             return false;
-
     }
     // Add mortgageAmount() for property
     public boolean mortgageProperty(Property prp, Player p) {
@@ -51,8 +47,6 @@ public class Bank {
         }
         else
             return false;
-
-
     }
     // Add mortgageAmount() for property
     public boolean unMortgageProperty(Property prp, Player p) {
@@ -63,7 +57,6 @@ public class Bank {
         }
         else
             return false;
-
     }
     // Add buildingCount parameter. HouseCost, hotelCost ,setRentAmount() for each property.
     public boolean buyBuilding(Player p, Building type, Property prp , int buildingCount) {
@@ -75,28 +68,22 @@ public class Bank {
                 prp.setNumOfHouse (buildingCount);
                 prp.setRentAmount();
                 return true;
-
             }
             else
                 return false;
         }
         if (type=="Hotel") {
-            if (p.hasMonopoly() && p.getBalance() >= (buildingCount * prp.hotelCost) && prp.getNumOfHouse () = 4)
+            if (p.hasMonopoly() && p.getBalance() >= (buildingCount * prp.hotelCost) && prp.getNumOfHouse() = 4)
             {
                 p.balance = p.balance - (buildingCount * prp.hotelCost);
                 numberOfHotels = numberOfHotels - buildingCount;
                 prp.setNumOfHotel (buildingCount);
                 prp.setRentAmount(); // Add setRentAmount function to Property class.
                 return true;
-
             }
             else
                 return false;
-
         }
-
-
-
     }
     public boolean tradeCard(int cardID, Player owner, Player target) {
         boolean flag = false;
@@ -111,8 +98,6 @@ public class Bank {
                 flag = false;
         }
         return flag;
-
-
     }
     public boolean tradeMoney(int moneyAmount, Player owner, Player target) {
         if (owner.getBalance () >= moneyAmount) {
@@ -122,24 +107,18 @@ public class Bank {
         }
         else
             return false;
-
-
     }
     public int getNumberOfHouses(){
         return this.numberOfHouses;
-
     }
     public void setNumberOfHouses(int numberOfHouses){
-
-        this.numberOfHouses= numberOfHouses;
+        this.numberOfHouses = numberOfHouses;
     }
     public int getNumberOfHotels(){
         return this.numberOfHotels;
-
     }
     public void setNumberOfHotels(int numberOfHotels){
-        this.numberOfHotels= numberOfHotels;
+        this.numberOfHotels = numberOfHotels;
     }
-
-
+    
 }
