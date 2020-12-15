36f29eb222d4b359c9a0e312f05e1f4c7a3daeb6
arnisafazla <arnisa.fazla@ug.bilkent.edu.tr>
2020-12-16 01:35:23 +0300

    back up deleted

--- backup/sample/subsystems/GameLogic/ElectricCompany.java
+++
@@ -1,7 +0,0 @@
-package sample.subsystems.GameLogic;
-
-public class ElectricCompany extends Utility{
-    public ElectricCompany(int value, int tLocation) {
-        super(value, 1, "Electric Company", tLocation);
-    }
-}
